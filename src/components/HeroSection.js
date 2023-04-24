ea3c628a178e5c06988fd53a217bce99596f8759
Aditya-Rajawat <adityarajawat04@gmail.com>
2023-04-24 17:41:04 +0530

    first commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,115 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { Button } from "../styles/Button";
+
+const HeroSection = ({ myData }) => {
+  const { name } = myData;
+
+  return (
+    <Wrapper className="main">
+      <div className="container">
+        <div className="grid grid-two-column">
+          <div className="hero-section-data">
+            <p className="intro-data">Welcome to </p>
+            <h1> {name} </h1>
+            <p>
+              Creative Scrapyard is a place where discarded materials are
+              transformed into works of art, inspiring a new wave of
+              eco-friendly creativity and sustainability.
+            </p>
+            <NavLink>
+              <Button className="shop">shop now</Button>
+            </NavLink>
+          </div>
+          {/* our homepage image  */}
+          <div className="hero-section-image">
+            <figure>
+              <img src="images/front.jpg" alt="" className="img-style" />
+            </figure>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  padding: 12rem 0;
+
+  img {
+    min-width: 10rem;
+    height: 10rem;
+    border-radius: 10px;
+  }
+
+  .hero-section-data {
+    p {
+      margin: 2rem 0;
+    }
+
+    h1 {
+      text-transform: capitalize;
+      font-weight: bold;
+    }
+
+    .intro-data {
+      margin-bottom: 0;
+      color: rgba(7, 7, 7, 0.66);
+      color: rgba(67, 203, 9, 0.8);
+    }
+
+    .shop {
+      background-color: rgba(46, 208, 14, 0.66);
+      background: #56ab2f;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #a8e063, #56ab2f);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #a8e063, #56ab2f); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+      border-radius: 10px;
+    }
+  }
+
+  .hero-section-image {
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  figure {
+    position: relative;
+
+    &::after {
+      content: "";
+      width: 60%;
+      height: 80%;
+      background-color: rgba(5, 5, 5, 0.66);
+      border-radius: 10px;
+      position: absolute;
+      left: 50%;
+      top: -5rem;
+      z-index: -1;
+    }
+  }
+  .img-style {
+    width: 100%;
+    height: auto;
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .grid {
+      gap: 10rem;
+    }
+
+    figure::after {
+      content: "";
+      width: 50%;
+      height: 100%;
+      left: 0;
+      top: 10%;
+      /* bottom: 10%; */
+      background-color: rgba(81, 56, 238, 0.4);
+    }
+  }
+`;
+
+export default HeroSection;
